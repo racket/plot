12bd81eb8176d46cfde03fafd96593cea1d1f1bb
Alex Harsanyi <AlexHarsanyi@gmail.com>
2018-02-20 19:28:50 +0800

    Bestow contracts upon the 2d-plot-snip% class
    ... but only for the methods that the user can call:
    `set-mouse-event-callback` and `set-overlay-renderers`

--- plot-gui-lib/plot/private/gui/snip2d.rkt
+++ plot-gui-lib/plot/private/gui/snip2d.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require racket/gui/base racket/class racket/match racket/list racket/math
+(require racket/gui/base racket/class racket/contract racket/match racket/list racket/math
          plot/private/common/math
          plot/private/common/format
          plot/private/common/ticks
@@ -9,11 +9,13 @@
          plot/private/common/parameter-group
          plot/private/common/draw-attribs
          plot/private/plot2d/plot-area
+         plot/private/plot2d/renderer
          plot/private/no-gui/plot2d-utils
+         plot/private/common/contract
          "worker-thread.rkt"
          "snip.rkt")
 
-(provide 2d-plot-snip% make-2d-plot-snip)
+(provide make-2d-plot-snip)
 
 (define update-delay 16)
 (define show-zoom-message? #t)
@@ -352,9 +354,18 @@
       (super resize w h))
     ))
 
+(define plot-mouse-event-callback/c (-> (is-a?/c snip%) (is-a?/c mouse-event%)
+                                        (or/c real? #f) (or/c real? #f) any/c))
+
+(define/contract 2d-plot-snip+c%
+  (class/c
+   (set-mouse-event-callback (->m (or/c plot-mouse-event-callback/c #f) any/c))
+   (set-overlay-renderers (->m (or/c (treeof renderer2d?) #f) any/c)))
+  2d-plot-snip%)
+
 (define (make-2d-plot-snip
          init-bm saved-plot-parameters
          make-bm plot-bounds-rect area width height)
-  (make-object 2d-plot-snip%
+  (make-object 2d-plot-snip+c%
     init-bm saved-plot-parameters
     make-bm plot-bounds-rect area width height))
