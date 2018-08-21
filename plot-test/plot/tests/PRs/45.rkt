ceb2fa9fca4cca6ab11884c81273cf2e8edc0632
Ben Greenman <benjaminlgreenman@gmail.com>
2018-08-21 16:59:01 -0400

    test: automatically close PR #45 frames after 5 seconds
    (so DrDr doesn't hang waiting for the user to quit manually!)

--- plot-test/plot/tests/PRs/45.rkt
+++ plot-test/plot/tests/PRs/45.rkt
@@ -1,12 +1,24 @@
 #lang racket/base
-(require plot)
+(require plot racket/class racket/gui/base)
 
-(plot-new-window? #t) ;; to regression-test #45
+(define show-frame/subspace
+  (let ([other-eventspace (make-eventspace)])
+    (lambda (make-frame [sleep-seconds 5])
+      (define f
+        (parameterize ([current-eventspace other-eventspace])
+          (make-frame)))
+      (send f show #t)
+      (sleep sleep-seconds)
+      (send f show #f))))
 
 ;; These plots should NOT have decorations. If they do, that means the parameter
 ;; is somehow ignored.
-(parameterize ((plot-decorations? #f))
-  (plot3d (surface3d (λ (x y) (* (cos x) (sin y)))
-                     -3.0 3.0 -3.0 3.0)))
-(parameterize ((plot-decorations? #f))
-  (plot (function sin) #:x-min -3 #:x-max 3))
+(show-frame/subspace
+  (lambda ()
+    (parameterize ((plot-decorations? #f))
+      (plot3d-frame (surface3d (λ (x y) (* (cos x) (sin y)))
+                               -3.0 3.0 -3.0 3.0)))))
+(show-frame/subspace
+  (lambda ()
+    (parameterize ((plot-decorations? #f))
+      (plot-frame (function sin) #:x-min -3 #:x-max 3))))
