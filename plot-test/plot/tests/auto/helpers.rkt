16c3070c39f88475003185e0955bffdc58b08046
Alex Harsanyi <AlexHarsanyi@gmail.com>
2020-07-10 17:51:58 +0800

    fixup pr59 tests too

--- plot-test/plot/tests/auto/helpers.rkt
+++ plot-test/plot/tests/auto/helpers.rkt
@@ -29,40 +29,6 @@
         (#t
          (equal? set1 set2))))
 
-#;(define (same-draw-commands? set1 set2)
-  (cond ((and (pair? set1) (pair? set2))
-         (and
-          (same-draw-commands? (car set1) (car set2))
-          (same-draw-commands? (cdr set1) (cdr set2))))
-        ((and (number? set1) (number? set2))
-         (let ([difference (abs (- set1 set2))])
-           (< difference 1e-4)))
-        (#t
-         (equal? set1 set2))))
-
-#;(define (same-draw-steps? set1 set2)
-  (for/and ([step1 (in-list set1)]
-            [step2 (in-list set2)])
-    (define command1 (car step1))
-    (define command2 (car step2))
-    (define result
-      (and (equal? command1 command2)
-           (case command1
-             ((draw-text)
-              (match-define (list 'draw-text text1 x1 y1 combine1? offset1 angle1) step1)
-              (match-define (list 'draw-text text2 x2 y2 combine2? offset2 angle2) step2)
-              (and (equal? text1 text2)
-                   (equal? combine1? combine2?)
-                   (let ([difference (abs (- offset1 offset2))])
-                     (< difference 1e-4))
-                   (let ([difference (abs (- angle1 angle2))])
-                     (< difference 1e-4))))
-             (else
-              (same-draw-commands? step1 step2)))))
-    (unless result
-      (printf "mismatch at ~a, ~a~%" step1 step2))
-    result))
-
 (define (check-draw-steps plot-function saved-steps-file)
   (define saved (call-with-input-file saved-steps-file read))
   (define current (plot-function generate-draw-steps))
