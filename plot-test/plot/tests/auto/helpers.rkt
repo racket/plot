7500fbb6312a4eeb2b59ec5af98db49bcd37f380
Alex Harsanyi <AlexHarsanyi@gmail.com>
2020-07-10 17:39:19 +0800

    try outsmarting the font system

--- plot-test/plot/tests/auto/helpers.rkt
+++ plot-test/plot/tests/auto/helpers.rkt
@@ -6,8 +6,15 @@
 (plot-width 1024)
 (plot-height 768)
 
+(define mock-record-dc%
+  (class record-dc%
+    (init)
+    (super-new)
+    (define/override (get-text-extent text (font #f) (combine? #f) (offset 0))
+      (values (* 10 (string-length text)) 10 0 0))))
+
 (define (generate-draw-steps renderer-tree)
-  (define dc (new record-dc% [width (plot-width)] [height (plot-height)]))
+  (define dc (new mock-record-dc% [width (plot-width)] [height (plot-height)]))
   (plot/dc renderer-tree dc 0 0 (plot-width) (plot-height))
   (send dc get-recorded-datum))
 
@@ -22,6 +29,40 @@
         (#t
          (equal? set1 set2))))
 
+#;(define (same-draw-commands? set1 set2)
+  (cond ((and (pair? set1) (pair? set2))
+         (and
+          (same-draw-commands? (car set1) (car set2))
+          (same-draw-commands? (cdr set1) (cdr set2))))
+        ((and (number? set1) (number? set2))
+         (let ([difference (abs (- set1 set2))])
+           (< difference 1e-4)))
+        (#t
+         (equal? set1 set2))))
+
+#;(define (same-draw-steps? set1 set2)
+  (for/and ([step1 (in-list set1)]
+            [step2 (in-list set2)])
+    (define command1 (car step1))
+    (define command2 (car step2))
+    (define result
+      (and (equal? command1 command2)
+           (case command1
+             ((draw-text)
+              (match-define (list 'draw-text text1 x1 y1 combine1? offset1 angle1) step1)
+              (match-define (list 'draw-text text2 x2 y2 combine2? offset2 angle2) step2)
+              (and (equal? text1 text2)
+                   (equal? combine1? combine2?)
+                   (let ([difference (abs (- offset1 offset2))])
+                     (< difference 1e-4))
+                   (let ([difference (abs (- angle1 angle2))])
+                     (< difference 1e-4))))
+             (else
+              (same-draw-commands? step1 step2)))))
+    (unless result
+      (printf "mismatch at ~a, ~a~%" step1 step2))
+    result))
+
 (define (check-draw-steps plot-function saved-steps-file)
   (define saved (call-with-input-file saved-steps-file read))
   (define current (plot-function generate-draw-steps))
@@ -32,7 +73,7 @@
     (define data-file (build-path
                        base
                        (string-append "new-" (path->string name))))
-    (call-with-output-file data-file (lambda (out) (write current out)))
+    (call-with-output-file data-file (lambda (out) (write current out)) #:exists 'replace)
     ;; Also generate an image of the current plot
     (define image-file (path-replace-extension data-file ".png"))
     (plot-function (lambda (rt) (plot-file rt image-file)))
