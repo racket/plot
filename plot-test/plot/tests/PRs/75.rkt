eb209b5c55541d95a0515b9c366fffddec214413
bdeket <bedeke@hotmail.com>
2020-09-14 17:46:47 +0200

    margin: fix PR number

--- plot-test/plot/tests/PRs/75.rkt
+++ plot-test/plot/tests/PRs/75.rkt
@@ -4,20 +4,20 @@
          racket/runtime-path
          "../helpers.rkt")
 
-;; Tests for: https://github.com/racket/plot/pull/76
+;; Tests for: https://github.com/racket/plot/pull/75
 
-(define (do-plot-vector-field output-fn)
+(define (do-plot-margin-calc output-fn)
   (output-fn (function values 0 1)
              #:title (text "a realy realy realy realy realy realy realy realy ridiculously high title" null 12 1.55)))
 
-(define-runtime-path pr76-data "./test-data/pr76.dat")
+(define-runtime-path pr75-data "./test-data/pr75.dat")
 
-(define pr76-test-suite
+(define pr75-test-suite
   (test-suite
-   "PR#76: margin error"
-   (test-case "pr76-margin"
-     (check-draw-steps do-plot-vector-field pr76-data))))
+   "PR#75: margin error"
+   (test-case "pr75-margin"
+     (check-draw-steps do-plot-margin-calc pr75-data))))
 
 (module+ test
   (require rackunit/text-ui)
-  (run-tests pr76-test-suite))
+  (run-tests pr75-test-suite))
